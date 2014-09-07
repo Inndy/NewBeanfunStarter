a37d2f7a0c007a95d518f3babc16108b4a99d305
Inndy <inndy.mail@gmail.com>
2014-09-08 05:43:54 +0800

    Define method of BeanfunClient

--- NewBeanfunStarter/NewBeanfun/BeanfunClient.cs
+++ NewBeanfunStarter/NewBeanfun/BeanfunClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +8,36 @@ namespace NewBeanfun
 {
     public class BeanfunClient
     {
+        public bool StillAlive { get; private set; }
+        private List<BeanfunGame> Games;
+        public BeanfunClient(List<BeanfunGame> Games)
+        {
+            this.Games = new List<BeanfunGame>(Games);
+        }
+
+        public bool Initialize()
+        {
+
+        }
+
+        public Image GetCaptchaImage()
+        {
+
+        }
+
+        public bool Login(string Account, string Password, string CaptchaCode)
+        {
+            this.StillAlive = true;
+        }
+
+        public List<BeanfunGame> ListGames()
+        {
+
+        }
+
+        public bool Ping()
+        {
+            this.StillAlive = true;
+        }
     }
 }
