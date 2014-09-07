59036e7493c25d6f8f100ec2eabbf2b66ecde3fd
Inndy <inndy.mail@gmail.com>
2014-09-08 02:40:43 +0800

    Add NewBeanfun classes

---
+++ NewBeanfunStarter/NewBeanfun/BeanfunGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewBeanfun
+{
+    public class BeanfunGame
+    {
+        public string Name { get; private set; }
+        public string ServiceCode { get; private set; }
+        public string ServiceRegion { get; private set; }
+        public string PPPPP { get; private set; }
+
+        public BeanfunGame(string Name, string ServiceCode, string ServiceRegion, string PPPPP = null)
+        {
+            this.Name = Name;
+            this.ServiceCode = ServiceCode;
+            this.ServiceRegion = ServiceRegion;
+            this.PPPPP = PPPPP;
+        }
+    }
+}
---
+++ NewBeanfunStarter/NewBeanfun/BeanfunGameAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewBeanfun
+{
+    public class BeanfunGameAccount
+    {
+        public string Account { get; private set; }
+        public string Number { get; private set; }
+        public string Name { get; private set; }
+        public BeanfunGame Game { get; private set; }
+
+        public BeanfunGameAccount(string Account, string Number, string Name, BeanfunGame Game)
+        {
+            this.Account = Account;
+            this.Number = Number;
+            this.Name = Name;
+            this.Game = Game;
+        }
+    }
+}
