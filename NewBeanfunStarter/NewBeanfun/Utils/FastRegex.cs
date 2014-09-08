b7351ac14ba8843830017216afd0ff2e485ccb22
Inndy <Inndy.mail@gmail.com>
2014-09-08 16:46:45 +0800

    Add util FastRegex

---
+++ NewBeanfunStarter/NewBeanfun/Utils/FastRegex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NewBeanfun.Utils
+{
+    public static class FastRegex
+    {
+        public static string Extract(string regexp, string data)
+        {
+            Regex reg = new Regex(regexp);
+            if (reg.IsMatch(data))
+            {
+                return reg.Match(data).Groups[1].Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
